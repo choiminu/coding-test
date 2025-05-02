ea4ffede2f4c806aa67425bb67644bdb0578e6de
최민우 <alsdn4590@gmail.com>
2025-05-02 12:41:47 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+
+            for (int j = 1; j <= N * 2 - (i * 2); j++) {
+                bw.write(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+
+            bw.write("\n");
+        }
+
+        for (int i = 1; i < N; i++) {
+
+            for (int j = 1; j <= N - i; j++) {
+                bw.write("*");
+            }
+
+            for (int j = 1; j <= i * 2; j++) {
+                bw.write(" ");
+            }
+
+            for (int j = 1; j <= N - i; j++) {
+                bw.write("*");
+            }
+
+            bw.write("\n");
+        }
+
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
