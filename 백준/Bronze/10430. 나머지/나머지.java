305e50a69063a31caa07d2c7b89b4ff0a3473e72
최민우 <alsdn4590@gmail.com>
2025-05-02 12:20:32 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 14928 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        bw.write((A + B) % C + "\n");
+        bw.write(((A % C) + (B%C)) % C + "\n");
+        bw.write((A * B) % C  + "\n");
+        bw.write(((A % C)) * (B % C) % C  + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
