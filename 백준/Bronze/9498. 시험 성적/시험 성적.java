c674d9388dabe6af9a3d279680996643ce936f0c
최민우 <alsdn4590@gmail.com>
2025-05-02 12:24:40 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int score = Integer.parseInt(br.readLine());
+        char grade = '?';
+
+        switch (score / 10) {
+            case 10:
+                grade = 'A';
+                break;
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            case 6:
+                grade = 'D';
+                break;
+            default:
+                grade = 'F';
+        }
+
+        bw.write(grade);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
