6933181aac4999fa2d679a1d0d668d24d1d9a2e7
최민우 <alsdn4590@gmail.com>
2025-05-04 13:56:17 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14320 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        char[] input = br.readLine().toCharArray();
-
+        int length = Integer.parseInt(br.readLine());
+        int r = 31;
+        int m = 1234567891;
         long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += (long) ((input[i] - 'a' + 1) * Math.pow(31, i));
-        }
+        long mod = 1;
+        char[] ch = br.readLine().toCharArray();
 
-        System.out.println(sum);
+        for(int i = 0; i < length; i++) {
+
+            sum += (((int)((ch[i] - 'a') + 1)) * mod);
+            mod = (r * mod) % m;
+        }
+        System.out.println(sum % m);
     }
 }
