597d6e16fa60d97082cb0cfabebb1e5d1535f9d0
최민우 <alsdn4590@gmail.com>
2025-05-07 21:47:05 +0900

    [Silver IV] Title: 수 찾기, Time: 1100 ms, Memory: 64608 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 class Main {
@@ -12,18 +9,83 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] A = new int[N];
         StringTokenizer token = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            map.put(Integer.parseInt(token.nextToken()), 1);
+            A[i] = Integer.parseInt(token.nextToken());
         }
 
         int M = Integer.parseInt(br.readLine());
+
+        int[] R = new int[M];
         token = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            System.out.println(map.getOrDefault(Integer.parseInt(token.nextToken()), 0));
+            R[i] = Integer.parseInt(token.nextToken());
         }
 
+        mergeSort(A, 0, A.length - 1);
+
+        for (int num : R) {
+            System.out.println(binarySearch(A, num));
+        }
+
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                return 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void mergeSort(int[] arr, int start, int end) {
+        if (start >= end) return;
+
+        int mid = (start + end) / 2;
+
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
+    }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int t = 0; t < temp.length; t++) {
+            arr[t + start] = temp[t];
+        }
     }
 
 }
