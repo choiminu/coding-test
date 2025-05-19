d85be6d89d67a834da31c0202865233e1a75b429
최민우 <alsdn4590@gmail.com>
2025-05-19 20:45:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1036 ms, Memory: 95456 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int cnt;
+    static int[] visited;
+    static List<List<Integer>> graph = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
+        int R = Integer.parseInt(token.nextToken());
+
+        visited = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            token = new StringTokenizer(br.readLine());
+
+            int U = Integer.parseInt(token.nextToken());
+            int V = Integer.parseInt(token.nextToken());
+
+            graph.get(U).add(V);
+            graph.get(V).add(U);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        cnt = 1;
+        bfs(R);
+
+        for (int i = 1; i < visited.length; i++) {
+            bw.write(visited[i] + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[node] = cnt++;
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            int newNode = queue.poll();
+
+            for (int i = 0; i < graph.get(newNode).size(); i++) {
+                int next = graph.get(newNode).get(i);
+
+                if (visited[next] != 0) continue;
+
+                queue.offer(next);
+                visited[next] = cnt++;
+            }
+        }
+
+    }
+
+
+}
