06f9e88701a765fae696548c6bd363f8af6be99e
최민우 <alsdn4590@gmail.com>
2025-05-20 23:10:32 +0900

    [Bronze V] Title: 이상한 기호, Time: 96 ms, Memory: 14300 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
+
+        int result = (N + M) * (N - M);
+
+        bw.write(result + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
