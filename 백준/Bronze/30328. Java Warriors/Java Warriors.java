ac2ff9ea3c80e231be4414da927e318ff152f27c
최민우 <alsdn4590@gmail.com>
2025-05-21 18:18:55 +0900

    [Bronze V] Title: Java Warriors, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        int i = Integer.parseInt(token.nextToken());
+
+        bw.write(i * 4000 + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
