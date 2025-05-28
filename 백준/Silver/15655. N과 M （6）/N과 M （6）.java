23898f0b8528c4ec97aea8cd8cdbfc7ca21c7280
최민우 <alsdn4590@gmail.com>
2025-05-28 18:56:12 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 14724 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static int[] answer;
+    static boolean[] isVisited;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        arr = new int[N];
+        answer = new int[M];
+        isVisited = new boolean[N];
+
+        token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(token.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs( 0, 0);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int start, int depth) throws IOException {
+
+        if (depth == M) {
+            for (int val : answer) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                answer[depth] = arr[i];
+                dfs(i + 1, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+
+}
