9080f7588f6d9cc0787978c3405befeda55bf46e
최민우 <alsdn4590@gmail.com>
2025-05-30 19:33:49 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+
+    static int[] A;
+    static int[] R;
+    static boolean[] isVisited;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        A = new int[N];
+        R = new int[M];
+        isVisited = new boolean[N];
+
+        token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(token.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        dfs(0, 0);
+
+        bw.write(sb.toString());
+
+
+        br.close();
+        bw.flush();
+        br.close();
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int val : R) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = start; i < N; i++) {
+            if (!isVisited[i] && before != A[i]) {
+                isVisited[i] = true;
+                before = A[i];
+                R[depth] = A[i];
+                dfs(i, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+
+    }
+}
