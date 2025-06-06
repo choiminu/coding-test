14c6cb206bdcd3c7b2d437404b015a55cd3a7c26
최민우 <alsdn4590@gmail.com>
2025-06-06 18:19:58 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 328 ms, Memory: 40024 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,24 +1,18 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
+import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
     static int N;
     static int K;
-
     static final int MAX = 100000;
-    static int[] time = new int[MAX + 1];
-    static int[] parent = new int[MAX + 1];
+
+    static boolean[] visited;
+    static int[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,50 +21,58 @@ class Main {
         StringTokenizer token = new StringTokenizer(br.readLine());
         N = Integer.parseInt(token.nextToken());
         K = Integer.parseInt(token.nextToken());
+        visited = new boolean[MAX + 1];
+        graph = new int[MAX + 1];
 
-        bfs();
+        int bfs = bfs();
 
         Stack<Integer> stack = new Stack<>();
         stack.push(K);
         int index = K;
 
         while (index != N) {
-            stack.push(parent[index]);
-            index = parent[index];
+            stack.push(graph[index]);
+            index = graph[index];
         }
 
-        bw.write(time[K] + "\n");
+        bw.write(bfs + "\n");
+
         while (!stack.isEmpty()) {
             bw.write(stack.pop() + " ");
         }
-
 
         br.close();
         bw.flush();
         bw.close();
     }
 
-    public static void bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(N);
-        time[N] = 0;
+    private static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{N, 1});
+        visited[N] = true;
+        graph[N] = 0;
 
         while (!queue.isEmpty()) {
-            Integer now = queue.poll();
+            int[] cur = queue.poll();
+            int current = cur[0];
+            int time = cur[1];
 
-            if (now == K) {
-                break;
+            if (current == K) {
+                return time - 1;
             }
 
-            int[] nextPositions = {now - 1, now + 1, now * 2};
-            for (int next : nextPositions) {
-                if (next >= 0 && next <= MAX && time[next] == 0) {
-                    queue.offer(next);
-                    time[next] = time[now] + 1;
-                    parent[next] = now;
+            for (int next : new int[]{current - 1, current + 1, current * 2}) {
+                if (next < 0 || next > MAX ||visited[next]) {
+                    continue;
                 }
+                queue.offer(new int[]{next, time + 1});
+                visited[next] = true;
+                graph[next] = current;
+
             }
         }
+        return -1;
     }
-}
 
+
+}
