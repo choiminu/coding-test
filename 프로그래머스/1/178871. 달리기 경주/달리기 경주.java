cd83af57087d2d67c0b97403187caa373fe602d7
최민우 <alsdn4590@gmail.com>
2025-06-15 14:09:22 +0900

    [level 1] Title: 달리기 경주, Time: 233.02 ms, Memory: 354 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,33 +1,24 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-
-        List<String> list = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
         
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
-            list.add(players[i]);
             map.put(players[i], i);
         }
         
-        for (String calling : callings) {
-            int cur = map.get(calling);
-            int next = cur - 1;
+        for (String call : callings) {
+            int index = map.get(call);
             
-            String frontPlayer = list.get(next);
+            String frontPlayer = players[index - 1];
+            players[index - 1] = players[index];
+            players[index] = frontPlayer;
             
-            list.set(next, calling);
-            list.set(cur, frontPlayer);
-            
-            map.put(calling, next);
-            map.put(frontPlayer, cur);
-            
+            map.put(call, index - 1);
+            map.put(frontPlayer, index);
         }
         
-        return list.toArray(new String[0]);
+        return players;
     }
 }
