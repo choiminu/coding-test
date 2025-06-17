174775589d9f7df3d8641aa153de01d82ee82b86
최민우 <alsdn4590@gmail.com>
2025-06-17 20:56:28 +0900

    [Silver I] Title: 1로 만들기 2, Time: 136 ms, Memory: 22140 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -13,39 +13,43 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N + 1];
-        int[] fw = new int[N + 1];
+        int[] prev = new int[N + 1];
 
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[1] = 0;
-        fw[1] = 1;
 
         for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
 
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            if (dp[i-1] + 1 < dp[i]) {
+                dp[i] = dp[i - 1] + 1;
+                prev[i] = i - 1;
             }
 
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            if (i % 3 == 0 && dp[i/3] + 1 < dp[i]) {
+                dp[i] = dp[i/3] + 1;
+                prev[i] = i / 3;
             }
+
+            if (i % 2 == 0 && dp[i/2] + 1 < dp[i]) {
+                dp[i] = dp[i/2] + 1;
+                prev[i] = i / 2;
+            }
+
         }
 
-        System.out.println(dp[N]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(dp[N]).append("\n");
 
         while (N > 0) {
-            System.out.print(N + " ");
-
-            if (N == 1) break;
-
-            if (N % 3 == 0 && dp[N/3] == dp[N] - 1) {
-                N /= 3;
-            } else if (N % 2 == 0 && dp[N/2] == dp[N] - 1) {
-                N /= 2;
-            } else {
-                N--;
-            }
+            sb.append(N).append(" ");
+            N = prev[N];
         }
 
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
 
