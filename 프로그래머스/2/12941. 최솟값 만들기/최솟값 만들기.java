095e94764c7ab732d9c255c3c9d05882488bc79a
최민우 <alsdn4590@gmail.com>
2025-06-19 16:48:39 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.73 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,14 +6,54 @@ class Solution
     {
         int answer = 0;
 
-        Arrays.sort(A);
-        Arrays.sort(B);
+        merge(A, 0, A.length - 1);
+        merge(B, 0, B.length - 1);
         
         for (int i = 0; i < A.length; i++) {
             answer += A[i] * B[A.length - 1 - i];
         }
 
         return answer;
+    }
+    
+    public void merge(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        
+        int mid = (right + left) / 2;
+        
+        merge(arr, left, mid);
+        merge(arr, mid + 1, right);
+        
+        mergeSort(arr, left, mid, right);
+    }
+    
+    public void mergeSort(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        
+        while(i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+        
+        while(i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        
+        while(j <= right) {
+            temp[k++] = arr[j++];
+        }
+        
+        for (int t = 0; t < temp.length; t++) {
+            arr[left + t] = temp[t];
+        }
     }
     
 }
