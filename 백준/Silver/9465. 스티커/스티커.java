52ac75bf6f4071cf5ceea1cba1ac5d2b3e7a6ea3
최민우 <alsdn4590@gmail.com>
2025-06-28 19:49:10 +0900

    [Silver I] Title: 스티커, Time: 676 ms, Memory: 112384 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -16,33 +16,27 @@ public class Main {
         int tc = Integer.parseInt(br.readLine());
 
         while (tc --> 0) {
-            int max = 0;
             int N = Integer.parseInt(br.readLine());
-
-            int[][] stickers = new int[2][N];
-            int[][] dp = new int[2][N];
+            int[][] stickers = new int[2][N + 1];
+            int[][] dp = new int[2][N + 1];
 
             for (int i = 0; i < 2; i++) {
                 StringTokenizer token = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
+                for (int j = 1; j <= N; j++) {
                     stickers[i][j] = Integer.parseInt(token.nextToken());
                 }
             }
 
-            dp[0][0] = stickers[0][0];
-            dp[1][0] = stickers[1][0];
+            dp[0][1] = stickers[0][1];
+            dp[1][1] = stickers[1][1];
 
-            if (N > 1) {
-                dp[0][1] = dp[1][0] + stickers[0][1];
-                dp[1][1] = dp[0][0] + stickers[1][1];
-            }
-
-            for (int i = 2; i < N; i++) {
+            for (int i = 2; i <= N; i++) {
                 dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + stickers[0][i];
                 dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + stickers[1][i];
             }
 
-            bw.write(Math.max(dp[0][N - 1], dp[1][N - 1]) + "\n");
+            int max = Math.max(dp[0][N], dp[1][N]);
+            bw.write(max + "\n");
 
         }
 
