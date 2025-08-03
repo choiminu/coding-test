aeb9ede5531d55d7025608d90eec9904b995dc61
최민우 <alsdn4590@gmail.com>
2025-08-03 16:08:11 +0900

    [Bronze III] Title: Automated Checking Machine, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10395. Automated Checking Machine/Automated Checking Machine.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] X = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] Y = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        String res = "Y";
+        for (int i = 0; i < 5; ++i) {
+            if (X[i] == Y[i]) {
+                res = "N";
+                break;
+            }
+        }
+
+        // Output
+        System.out.println(res);
+    }
+}
