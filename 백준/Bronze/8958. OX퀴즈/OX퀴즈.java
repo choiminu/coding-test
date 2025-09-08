8e9e0fca8eb6687bef26b09be48278280e2a2907
최민우 <alsdn4590@gmail.com>
2025-09-09 06:46:54 +0900

    [Bronze II] Title: OX퀴즈, Time: 120 ms, Memory: 16184 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -3,40 +3,34 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            char[] input = br.readLine().toCharArray();
+        int T = Integer.parseInt(br.readLine());
 
-            int sum = 0;
-            int score = 0;
-            for (char c : input) {
-                if (c == 'O') {
-                    score++;
-                    sum += score;
+        while (T-- > 0) {
+            int totalScore = 0;
+            int score = 1;
+
+            char[] res = br.readLine().toCharArray();
+            for (char ch : res) {
+                if (ch == 'O') {
+                    totalScore += score++;
                 } else {
-                    score = 0;
+                    score = 1;
                 }
             }
 
-            bw.write(sum + "\n");
-
+            bw.write(totalScore + "\n");
         }
 
         br.close();
         bw.flush();
         bw.close();
-
     }
-
 }
 
