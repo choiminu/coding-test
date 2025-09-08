efe5a1edad29957f6e1f94975b801ce32eedfebf
최민우 <alsdn4590@gmail.com>
2025-09-08 16:13:52 +0900

    [Bronze V] Title: 검증수, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -3,31 +3,27 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-class Main {
 
+public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int result = 0;
-        while (token.hasMoreTokens()) {
-            result += (int) Math.pow(Integer.parseInt(token.nextToken()), 2);
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
         }
 
-        bw.write(result % 10 +"");
+        bw.write(sum % 10 + "");
 
         br.close();
         bw.flush();
         bw.close();
-
     }
-
 }
 
