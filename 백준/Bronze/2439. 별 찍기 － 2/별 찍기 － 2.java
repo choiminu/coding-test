79b7b983d0c02ff6f244a80b19ed67bcc8b36f8d
최민우 <alsdn4590@gmail.com>
2025-09-09 05:44:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -3,21 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= N; i++) {
-            bw.write(" ".repeat(N - i));
-            bw.write("*".repeat(i) + "\n");
+            for (int j = i + 1; j <= N; j++) {
+                sb.append(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
         }
+
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
