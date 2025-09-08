99aea32ae5dac9ca7fe01ceb41667b0756b2f17e
최민우 <alsdn4590@gmail.com>
2025-09-08 16:18:17 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,16 +3,19 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= 9; i++) {
-            bw.write(a + " * " + i + " = " + (a * i) + "\n");
+            bw.write(N + " * " + i + " = " + (N*i) + "\n");
         }
 
         br.close();
@@ -20,3 +23,4 @@ public class Main {
         bw.close();
     }
 }
+
