418553196cf048eeca3f32267adcfd6d7c1bfbc3
최민우 <alsdn4590@gmail.com>
2025-09-08 16:19:04 +0900

    [Bronze V] Title: N 찍기, Time: 244 ms, Memory: 21456 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -3,18 +3,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-class Main {
 
+public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+
         for (int i = 1; i <= N; i++) {
             bw.write(i + "\n");
         }
@@ -22,7 +21,6 @@ class Main {
         br.close();
         bw.flush();
         bw.close();
-
     }
 }
 
