b5b228e683167eac4f1243ee5d8b2996544a4f22
최민우 <alsdn4590@gmail.com>
2025-09-08 16:22:53 +0900

    [Bronze V] Title: 고양이, Time: 104 ms, Memory: 13976 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("\\    /\\\n"
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\\n"
                 + " )  ( ')\n"
                 + "(  /  )\n"
                 + " \\(__)|");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+
