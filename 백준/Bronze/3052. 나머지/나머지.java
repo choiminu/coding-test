0cb78a355a01cadfbc70cf4d6064264c185dc9d4
최민우 <alsdn4590@gmail.com>
2025-09-09 06:43:25 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -3,34 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] st = new boolean[42];
+        boolean[] remain = new boolean[42];
 
+        int sum = 0;
         for (int i = 0; i < 10; i++) {
-            st[Integer.parseInt(br.readLine()) % 42] = true;
-        }
+            int value = Integer.parseInt(br.readLine());
+            int r = value % 42;
 
-        int result = 0;
-
-        for (int i = 0; i < st.length; i++) {
-            if (st[i]) {
-                result++;
+            if (!remain[r]) {
+                sum++;
             }
+
+            remain[r] = true;
         }
 
-        bw.write(result + "");
-
+        bw.write(sum + "");
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
