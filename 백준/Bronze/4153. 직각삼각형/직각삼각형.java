3897ce16734445f992729c45e5bf7c9b4a0d956a
최민우 <alsdn4590@gmail.com>
2025-09-09 07:06:14 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,34 +1,40 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-    
-        Scanner in = new Scanner(System.in);
-       
-        
-        while(true) {
-        	
-        	int x = in.nextInt();
-        	int y = in.nextInt();
-        	int z = in.nextInt();
-        	
-        	if(x == 0 && y == 0 && z == 0) break;
-        	
-        	if((x * x + y * y) == z * z) {
-				System.out.println("right");
-			}
-        	else if(x * x == (y * y + z * z)) {
-				System.out.println("right");
-			}
-        	else if(y * y == (z * z + x * x)) {
-				System.out.println("right");
-			}
-        	else {
-				System.out.println("wrong");
-			}
-        	
-		}
- 
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int A = (int)Math.pow(Integer.parseInt(st.nextToken()),2);
+            int B = (int)Math.pow(Integer.parseInt(st.nextToken()),2);
+            int C = (int)Math.pow(Integer.parseInt(st.nextToken()),2);
+
+            if (A == 0 && B == 0 && C == 0) {
+                break;
+            }
+
+            int max = Math.max(Math.max(A,B), Math.max(A,C));
+
+            if (A + B + C - max == max) {
+                bw.write("right\n");
+            } else {
+                bw.write("wrong\n");
+            }
+
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
+
