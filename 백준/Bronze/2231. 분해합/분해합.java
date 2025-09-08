552b87dce74199a82a4c02df9bbdba7fed0b46bd
최민우 <alsdn4590@gmail.com>
2025-09-09 08:00:39 +0900

    [Bronze II] Title: 분해합, Time: 140 ms, Memory: 14880 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == N) {
+                bw.write(i + "");
+                bw.flush();
+                return;
+            }
+        }
+
+        bw.write("0");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+
