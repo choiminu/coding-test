ccf156411a4082b21cbe858c2a234185a207b5b7
최민우 <alsdn4590@gmail.com>
2025-09-08 16:20:52 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -3,19 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int year = Integer.parseInt(br.readLine());
-        boolean flag = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        int N = Integer.parseInt(br.readLine());
 
-        bw.write(flag ? (1 + "") : (0 + ""));
+        int result = 0;
+        if (N % 4 == 0 && (N % 100 != 0 || N % 400 == 0)) {
+            result = 1;
+        }
+
+        bw.write(result+"");
+
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
