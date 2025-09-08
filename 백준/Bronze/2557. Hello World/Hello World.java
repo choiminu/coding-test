477f9613fb34abb7fbd40c615ce95403e792d45f
최민우 <alsdn4590@gmail.com>
2025-09-08 16:17:14 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+
