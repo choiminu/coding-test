d45de4ce0091e7fe935f89e0c3508180b85ad6bc
최민우 <alsdn4590@gmail.com>
2025-09-08 16:23:14 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("|\\_/|\n"
-                + "|q p|   /}\n"
-                + "( 0 )\"\"\"\\\n"
-                + "|\"^\"`    |\n"
-                + "||_/=\\\\__|");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+       bw.write("|\\_/|\n"
+               + "|q p|   /}\n"
+               + "( 0 )\"\"\"\\\n"
+               + "|\"^\"`    |\n"
+               + "||_/=\\\\__|");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
+
