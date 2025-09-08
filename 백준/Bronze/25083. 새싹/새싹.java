4eea2f4ea5309083e2941d4d229a61fc4c4112ea
최민우 <alsdn4590@gmail.com>
2025-09-09 05:40:17 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -3,9 +3,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -16,9 +19,9 @@ public class Main {
                 + "      |\n"
                 + "      |");
 
-
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
