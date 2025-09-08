5be7c9f65a80cfdd7a199d317d8a646c100251d3
최민우 <alsdn4590@gmail.com>
2025-09-09 06:07:30 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 16440 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,21 +1,27 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int tc = Integer.parseInt(br.readLine());
-        for (int i = 0; i < tc; i++) {
-            String[] input = br.readLine().split(" ");
-            int H = Integer.parseInt(input[0]);
-            int W = Integer.parseInt(input[1]);
-            int N = Integer.parseInt(input[2]);
+        int T = Integer.parseInt(br.readLine());
 
-            int floor = (N % H == 0) ? H : N % H;
-            int number = (N % H == 0) ? (N / H) : (N / H) + 1;
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
 
-            bw.write(String.format("%d%02d\n", floor, number));
+            int height = (N % H == 0) ? (H * 100) : (N % H * 100);
+            int width = (N % H == 0) ? (N / H) : (N / H + 1);
+
+            bw.write(height + width + "\n");
         }
 
         br.close();
@@ -23,3 +29,4 @@ public class Main {
         bw.close();
     }
 }
+
