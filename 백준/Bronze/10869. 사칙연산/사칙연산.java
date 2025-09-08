4a3605e0ad7d810ec42ec0520c47d60f5d6403fc
최민우 <alsdn4590@gmail.com>
2025-09-08 16:24:37 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -5,25 +5,28 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        bw.write(x + y + "\n");
+        bw.write(x - y + "\n");
+        bw.write(x * y + "\n");
+        bw.write(x / y + "\n");
+        bw.write(x % y + "\n");
 
-        bw.write((a+b) + "\n");
-        bw.write((a-b) + "\n");
-        bw.write((a*b) + "\n");
-        bw.write((a/b) + "\n");
-        bw.write((a%b) + "\n");
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
