d6eec630b0b2f42406e4f66910f04d72a9d07121
최민우 <alsdn4590@gmail.com>
2025-09-09 05:39:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 112 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -3,18 +3,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        char input = br.readLine().charAt(0);
+        String input = br.readLine();
 
-        bw.write((int) input + "");
+        bw.write((int)input.charAt(0) +"");
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
+
