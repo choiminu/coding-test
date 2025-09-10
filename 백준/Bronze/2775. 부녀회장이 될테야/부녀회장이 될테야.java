25bdd69f9709fa594199c91cadd541f7bccc2009
최민우 <alsdn4590@gmail.com>
2025-09-10 11:02:07 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,35 +1,49 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.function.BiFunction;
 
-class Main {
+
+public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int tc = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < tc; i++) {
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-
-            int[][] apt = new int[k + 1][n];
-
-            for (int y = 0; y < k + 1; y++) {
-                for (int x = 0; x < n; x++) {
-                    apt[y][0] = 1;
-                    apt[0][x] = x + 1;
+        int[][] apart = new int[15][15];
+        for (int i = 0; i < apart.length; i++) {
+            if (i == 0) {
+                for (int j = 0; j < apart[i].length; j++) {
+                    apart[i][j] = j+1;
+                }
+            } else {
+                for (int j = 0; j < apart[i].length; j++) {
+                    if (j == 0) {
+                        apart[i][j] = 1;
+                    }else {
+                        apart[i][j] = apart[i - 1][j] + apart[i][j-1];
+                    }
                 }
             }
-
-            for (int y = 1; y < k + 1; y++) {
-                for (int x = 1; x < n; x++) {
-                    apt[y][x] = apt[y - 1][x] + apt[y][x - 1];
-                }
-            }
-
-            System.out.println(apt[k][n-1]);
-
         }
+
+        int T = Integer.parseInt(br.readLine());
+        while (T --> 0) {
+            int y = Integer.parseInt(br.readLine());
+            int x = Integer.parseInt(br.readLine());
+
+            bw.write(apart[y][x-1] + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
     }
+
 }
 
