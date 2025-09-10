13b0146680334ea38662679f6a43c4b8c2acc251
최민우 <alsdn4590@gmail.com>
2025-09-11 08:53:12 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -1,40 +1,54 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.function.Predicate;
 
-class Main {
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] arr = new String[4];
-        int[] nums = new int[4];
+        Predicate<String> p = s -> {
+            try {
+                Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
 
-        int index = -1;
-        int num = 0;
-
+        int prev = 0;
         for (int i = 0; i < 3; i++) {
-            arr[i] = br.readLine();
-
-            if (arr[i].charAt(0) >= '0' && arr[i].charAt(0) <= '9') {
-                index = i;
-                num = Integer.parseInt(arr[i]);
+            String input = br.readLine();
+            if (p.test(input)) {
+                prev = Integer.parseInt(input);
+            } else {
+                if (prev != 0) {
+                    prev++;
+                }
             }
         }
 
+        prev++;
 
-        int count = 0;
-        for (int i = index; i < nums.length; i++, count++) {
-            nums[i] = num + count;
-        }
-
-        if (nums[3] % 3 == 0 && nums[3] % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (nums[3] % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (nums[3] % 5 == 0) {
-            System.out.println("Buzz");
+        if (prev % 3 == 0 && prev % 5 == 0) {
+            bw.write("FizzBuzz");
+        } else if (prev % 3 == 0) {
+            bw.write("Fizz");
+        } else if (prev % 5 == 0) {
+            bw.write("Buzz");
         } else {
-            System.out.println(nums[3]);
+            bw.write(prev+"");
         }
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
+
+
 }
+
