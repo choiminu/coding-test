0b12a6b615e6769c0ac8a81f7bebc3514d27ee12
최민우 <alsdn4590@gmail.com>
2025-09-12 08:42:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 632 ms, Memory: 49792 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] pos = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            pos[i][0] = Integer.parseInt(st.nextToken());
+            pos[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(pos, (p1, p2) -> {
+            if (p1[0] == p2[0]) {
+                return Integer.compare(p1[1], p2[1]);
+            }
+            return Integer.compare(p1[0], p2[0]);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] p : pos) {
+            sb.append(p[0]).append(" ").append(p[1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
+
