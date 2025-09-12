9efc933426621f73562bac52081b55abc07a9990
최민우 <alsdn4590@gmail.com>
2025-09-12 09:04:37 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 43132 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,91 +1,69 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
 
-        int N = Integer.parseInt(br.readLine());
+    static BiPredicate<int[], Integer> p = (arr, target) -> {
+        int left = 0;
+        int right = arr.length - 1;
 
-        int[] A = new int[N];
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(token.nextToken());
-        }
-
-        int M = Integer.parseInt(br.readLine());
-
-        int[] R = new int[M];
-        token = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            R[i] = Integer.parseInt(token.nextToken());
-        }
-
-        mergeSort(A, 0, A.length - 1);
-
-        for (int num : R) {
-            System.out.println(binarySearch(A, num));
-        }
-
-    }
-
-    public static int binarySearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
             if (arr[mid] == target) {
-                return 1;
-            } else if (arr[mid] < target) {
-                low = mid + 1;
-            }else {
-                high = mid - 1;
+                return true;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+
+            if (arr[mid] > target) {
+                right = mid - 1;
             }
         }
 
-        return 0;
-    }
+        return false;
+    };
 
-    public static void mergeSort(int[] arr, int start, int end) {
-        if (start >= end) return;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int mid = (start + end) / 2;
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid + 1, end);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        merge(arr, start, mid, end);
-    }
+        Arrays.sort(arr);
 
-    private static void merge(int[] arr, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= end) {
-            if (arr[i] < arr[j]) {
-                temp[k++] = arr[i++];
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            if(p.test(arr, Integer.parseInt(st.nextToken()))) {
+                sb.append("1\n");
             } else {
-                temp[k++] = arr[j++];
+                sb.append("0\n");
             }
         }
 
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
+        bw.write(sb.toString());
 
-        while (j <= end) {
-            temp[k++] = arr[j++];
-        }
-
-        for (int t = 0; t < temp.length; t++) {
-            arr[t + start] = temp[t];
-        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
