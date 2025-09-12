11de89b8cacd2b24edfeaf2890e9a0c5b5fee1fa
최민우 <alsdn4590@gmail.com>
2025-09-12 10:55:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1364 ms, Memory: 144232 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,40 +3,73 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        BiFunction<int[], Integer, Integer> lowwerBound = (arr, target) -> {
+            int left = 0;
+            int right = arr.length;
+            int mid = 0;
+            while (left < right) {
+                mid = (left + right) / 2;
+
+                if (arr[mid] >= target) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        };
+
+        BiFunction<int[], Integer, Integer> upperBound = (arr, target) -> {
+            int left = 0;
+            int right = arr.length;
+            int mid = 0;
+            while (left < right) {
+                mid = (left + right) / 2;
+
+                if (arr[mid] > target) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        };
+
         int N = Integer.parseInt(br.readLine());
-
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        N = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+
+        int T = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
+        StringBuilder sb = new StringBuilder();
+        while (st.hasMoreTokens()) {
+
             int num = Integer.parseInt(st.nextToken());
-            Integer result = map.getOrDefault(num, 0);
-            bw.write(result + " ");
+            int low = lowwerBound.apply(arr, num);
+            int high = upperBound.apply(arr, num);
+
+            sb.append(high - low).append(" ");
+
         }
+
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
@@ -44,3 +77,4 @@ public class Main {
     }
 
 }
+
