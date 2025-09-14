e77a744a01483696329a67b1a899d2d754c21e20
최민우 <alsdn4590@gmail.com>
2025-09-15 07:32:53 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 20800 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,73 +1,76 @@
-import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        BiConsumer<Stack<Integer>, Integer> push = (stack, value) -> {
+            stack.push(value);
+        };
+
+        Function<Stack<Integer>, Integer> pop = stack -> {
+            if (stack.isEmpty()) {
+                return -1;
+            }
+            return stack.pop();
+        };
+
+        Function<Stack<Integer>, Integer> size = stack -> {
+            return stack.size();
+        };
+
+        Function<Stack<Integer>, Integer> empty = stack-> {
+            return stack.isEmpty() ? 1 : 0;
+        };
+
+        Function<Stack<Integer>, Integer> top = stack-> {
+            return stack.isEmpty() ? -1 : stack.peek();
+        };
+
         int N = Integer.parseInt(br.readLine());
-        Stack stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
 
-            switch (input[0]) {
-                case "push" :
-                    stack.push(Integer.parseInt(input[1]));
+            String command = input[0];
+
+            switch (command) {
+                case "push":
+                    push.accept(stack, Integer.parseInt(input[1]));
                     break;
                 case "pop":
-                    bw.write(stack.pop() + "\n");
+                    sb.append(pop.apply(stack)).append("\n");
                     break;
                 case "size":
-                    bw.write(stack.size() + "\n");
+                    sb.append(size.apply(stack)).append("\n");
                     break;
-                case "empty" :
-                    bw.write(stack.isEmpty() + "\n");
+                case "empty":
+                    sb.append(empty.apply(stack)).append("\n");
                     break;
-                case "top" :
-                    bw.write(stack.top() + "\n");
+                case "top":
+                    sb.append(top.apply(stack)).append("\n");
                     break;
             }
-        }
+        };
+
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
         bw.close();
     }
 
-    static class Stack {
-        int[] arr = new int[10000];
-        int top = 0;
-
-        public void push(int data) {
-            arr[top++] = data;
-        }
-
-        public int pop() {
-            if (isEmpty() == 1) {
-                return  -1;
-            }
-            return arr[--top];
-        }
-
-        public int size() {
-            return top;
-        }
-
-        public int isEmpty() {
-            return top == 0 ? 1 : 0;
-        }
-
-        public int top() {
-            if (isEmpty() == 1) {
-                return  -1;
-            }
-            return arr[top - 1];
-        }
-
-    }
-
 }
+
