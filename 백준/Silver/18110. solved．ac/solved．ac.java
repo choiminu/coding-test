68dcf001e54f24d47bdac58f3f0394d0283c02f0
최민우 <alsdn4590@gmail.com>
2025-09-15 07:50:55 +0900

    [Silver IV] Title: solved.ac, Time: 464 ms, Memory: 41304 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,73 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
 
-        int[] A = new int[N];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = Integer.parseInt(br.readLine());
-        }
+		Collections.sort(list);
 
-        mergeSort(A, 0, A.length - 1);
+		int delete = (int) Math.round(N * 0.15);
 
-        int ceil = (int) Math.round(N * 0.15);
+		int sum = 0;
+		for (int i = delete; i < N - delete; i++) {			
+			sum += list.get(i);
+		}
 
-        int sum = 0;
-        for (int i = ceil; i < A.length - ceil; i++) {
-            sum += A[i];
-        }
-
-        System.out.println((int) Math.round((double) sum / (A.length - (ceil * 2))));
-
-
-    }
-
-    public static void mergeSort(int[] arr, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid + 1, end);
-
-        merge(arr, start, mid, end);
-    }
-
-    private static void merge(int[] arr, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= end) {
-            if (arr[i] < arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-            }
-        }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        while (j <= end) {
-            temp[k++] = arr[j++];
-        }
-
-        for (int t = 0; t < temp.length; t++) {
-            arr[t + start] = temp[t];
-        }
-    }
-
+		
+		double avg = (double) sum / (N - (delete * 2));
+		System.out.println(Math.round(avg));
+	}
 }
-
