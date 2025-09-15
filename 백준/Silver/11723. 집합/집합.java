48a6246ba73487bf6ea78044ac22832dc95e2d67
최민우 <alsdn4590@gmail.com>
2025-09-15 10:55:46 +0900

    [Silver V] Title: 집합, Time: 1164 ms, Memory: 307512 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -3,14 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-import java.util.StringTokenizer;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class Main {
 
@@ -19,71 +11,36 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-        Map<Integer, Integer> map = new HashMap<>();
-
-        Consumer<Integer> add = (key) -> {
-            if (!map.containsKey(key)) {
-                map.put(key, key);
-            }
-        };
-
-        Consumer<Integer> remove = (key) -> {
-            map.remove(key);
-        };
-
-        Consumer<Integer> check = (key) -> {
-            sb.append(map.containsKey(key) ? "1" : 0).append("\n");
-        };
-
-        Consumer<Integer> toggle = (key) -> {
-            if (map.containsKey(key)) {
-                map.remove(key);
-            } else {
-                map.put(key, key);
-            }
-        };
-
-        Runnable all = () -> {
-            for (int i = 1; i <= 20; i++) {
-                map.put(i, i);
-            }
-        };
-
-        Runnable empty = () -> {
-            for (int i = 1; i <= 20; i++) {
-                map.remove(i);
-            }
-        };
-
-        int N = Integer.parseInt(br.readLine());
-
-        while (N --> 0) {
+        int bit = 0;
+        while (T --> 0) {
             String[] input = br.readLine().split(" ");
 
             switch (input[0]) {
                 case "add":
-                    add.accept(Integer.parseInt(input[1]));
+                    bit |= 1 << Integer.parseInt(input[1]);
                     break;
                 case "remove":
-                    remove.accept(Integer.parseInt(input[1]));
+                    bit &= ~(1 << Integer.parseInt(input[1]));
                     break;
                 case "check":
-                    check.accept(Integer.parseInt(input[1]));
+                    sb.append((bit & 1 << Integer.parseInt(input[1])) != 0 ? "1" : "0").append("\n");
                     break;
                 case "toggle":
-                    toggle.accept(Integer.parseInt(input[1]));
+                    bit ^= 1 << Integer.parseInt(input[1]);
                     break;
                 case "all":
-                    all.run();
+                    bit = (1 << 21) - 1;
                     break;
                 case "empty":
-                    empty.run();
+                    bit = 0;
                     break;
             }
         }
 
         bw.write(sb.toString());
+
 
         br.close();
         bw.flush();
