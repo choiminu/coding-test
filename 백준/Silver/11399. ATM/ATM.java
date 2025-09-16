b4aa7cfd0e7f7afd0c0f1b1811e717130c51a56e
최민우 <alsdn4590@gmail.com>
2025-09-16 10:20:08 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -6,34 +6,47 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        for (int i = 0; i < A.length; i++) {
-            A[i] = Integer.parseInt(token.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(A);
+        // 각 사람이 돈을 인출하는데 걸리는 시간을 오름차순으로 정렬
+        Arrays.sort(arr);
 
-        int sum = A[0];
-        for (int i = 1; i < A.length; i++) {
-            A[i] = A[i - 1] + A[i];
-            sum += A[i];
+        // 직전까지의 누적 대기 시간
+        int prev = 0;
+
+        // 전체 대기 시간 합
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            // 현재 사람까지의 누적 시간
+            prev += arr[i];
+
+            // 전체 합계에 더하기
+            total += prev;
         }
 
-        bw.write(sum+"");
+        sb.append(total);
+
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
         bw.close();
-
     }
+
 }
 
