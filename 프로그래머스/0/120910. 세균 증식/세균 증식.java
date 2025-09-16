54d155646b1d61ae8da42b5e6b48a719c94ca420
최민우 <alsdn4590@gmail.com>
2025-09-16 21:46:49 +0900

    [level 0] Title: 세균 증식, Time: 1.09 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,8 +1,7 @@
 import java.util.stream.IntStream;
 class Solution {
     public int solution(int n, int t) {
-        int answer = IntStream.range(0, t)
-            .reduce(n, (i1, i2) ->  i1 * 2);
+        int answer = IntStream.range(0, t).reduce(n, (i1, i2) ->  i1 * 2);
         return answer;
     }
 }
