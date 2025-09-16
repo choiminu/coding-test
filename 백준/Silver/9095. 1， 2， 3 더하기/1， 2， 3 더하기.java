9276c655051cdf5898469f2c0cd41e7b1e56be37
최민우 <alsdn4590@gmail.com>
2025-09-16 11:29:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,30 +1,41 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-    static int[] dp = new int[11];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= 10; i++) {
-            recur(i);
-        }
+        int[] dp = new int[11];
+        dp[1] = 1; // 1
+        dp[2] = 2; // (1+1), (2)
+        dp[3] = 4; // (1+1+1), (1+2), (2+1), (3)
 
+        StringBuilder sb = new StringBuilder();
         while (T --> 0) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(dp[n]);
+            int N = Integer.parseInt(br.readLine());
+
+            for (int i = 4; i <= N; i++) {
+                if (dp[i] != 0) continue;
+                dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            }
+
+            sb.append(dp[N]).append("\n");
         }
 
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
-    public static int recur(int n) {
-        if (n == 1) return dp[n] = 1;
-        if (n == 2) return dp[n] = 2;
-        if (n == 3) return dp[n] = 4;
 
-        if (dp[n] != 0) return dp[n];
-        return dp[n] = recur(n - 1) + recur(n - 2) + recur(n - 3);
-    }
 }
+
