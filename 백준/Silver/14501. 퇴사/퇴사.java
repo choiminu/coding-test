425b235325e7e57dea42c249e31a0a27147a400b
최민우 <alsdn4590@gmail.com>
2025-09-17 19:37:15 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,25 +11,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int day = Integer.parseInt(br.readLine());
+        // 사용자에게 남은 N일을 입력받는다.
+        int N = Integer.parseInt(br.readLine());
 
-        int[][] schedule = new int[day + 1][2];
-        for (int i = 1; i <= day; i++) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
-            schedule[i][0] = Integer.parseInt(token.nextToken());
-            schedule[i][1] = Integer.parseInt(token.nextToken());
+        // 상담 시간을 저장할 배열 선언
+        int[] times = new int[N + 2];
+
+        // 상담 비용을 저장할 배열 선언
+        int[] pay = new int[N + 2];
+
+        // 1부터 N까지 순회하며 사용자에게 입력받은 상딤시간과 비용을 배열에 저장한다.
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            times[i] = Integer.parseInt(st.nextToken());
+            pay[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[day + 2];
-        for (int i = day; i >= 1; i--) {
-            int nextDay = i + schedule[i][0];
+        // N 일에서 얻을 수 있는 최대 수익을 보관할 배열 선언
+        int[] dp = new int[N + 2];
 
-            if (nextDay <= day + 1) {
-                dp[i] = Math.max(dp[i + 1], schedule[i][1] + dp[nextDay]);
+        for (int i = N; i >= 1; i--) {
+
+            // 오늘날짜에서 상담을 끝낸 다음 시작할 날짜
+            int next = i + times[i];
+
+            // 상담을 끝낸 날짜가 N보다 크거나 작은 경우는 상담이 가능하다.
+            if (next <= N + 1) {
+                dp[i] = Math.max(pay[i] + dp[times[i] + i], dp[i + 1]);
             } else {
                 dp[i] = dp[i + 1];
             }
-
         }
 
         System.out.println(dp[1]);
@@ -39,4 +49,7 @@ public class Main {
         bw.flush();
         bw.close();
     }
+
+
 }
+
