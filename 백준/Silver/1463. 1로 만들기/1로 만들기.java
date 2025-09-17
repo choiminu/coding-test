1a886065a59a56ee55a169612c35b47c05f8e5b0
최민우 <alsdn4590@gmail.com>
2025-09-17 16:48:26 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,36 +4,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 사용자에게 정수 N을 입력받는다.
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        // 상태를 보관할 dp 배열 선언 (정수 n을 1로 만들기 위한 연산의 최소횟수)
-        int[] dp = new int[n + 1];
+        // 연산의 최소 횟수를 보관한다.
+        int[] dp = new int[N + 1];
 
-        // dp[0] 과 dp[1]은 연산 횟수가 1이니 2부터 순회
-        for (int i = 2; i < dp.length; i++) {
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
 
-            // 1을 빼는 경우
+        for (int i = 2; i <= N; i++) {
+            // 현재 숫자에서 1을 빼면 [i - 1] 값이 되기 때문에 이전 연산 횟수에 1을 더해준다.
             dp[i] = dp[i - 1] + 1;
 
-            // 2로 나누어 떨어지는 경우
+            // 현재 숫자가 2로 나뉘어 떨어진다면?
             if (i % 2 == 0) {
+                // 현재 숫자에서 1을 뺀 연산 횟수와 2로 나눌때 연산 횟수 중에 더 작은 값을 저장한다.
+                // 1을 뺀 연산은 위에서 이미 +1로 연산 횟수를 기록하였기 때문에 2으로 나누는 경우에도 +1로 연산횟수를 기록한다.
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
 
-            // 3로 나누어 떨어지는 경우
+            // 현재 숫자가 3로 나뉘어 떨어진다면?
             if (i % 3 == 0) {
+                // 현재 숫자에서 1을 뺀 연산 횟수와 3로 나눌때 연산 횟수 중에 더 작은 값을 저장한다.
+                // 1을 뺀 연산은 위에서 이미 +1로 연산 횟수를 기록하였기 때문에 3으로 나누는 경우에도 +1로 연산횟수를 기록한다.
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
 
-        bw.write(dp[n]+"");
+        System.out.println(dp[N]);
 
         br.close();
         bw.flush();
