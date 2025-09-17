06719db11912086afe299556e7abcd10b5cdcae2
최민우 <alsdn4590@gmail.com>
2025-09-18 08:50:35 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,14 +2,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    static int n, m;
+    static int X, Y;
     static int[][] map;
     static boolean[][] isVisited;
 
@@ -18,53 +18,63 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(token.nextToken());
-        m = Integer.parseInt(token.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Y = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
-        isVisited = new boolean[n][m];
+        map = new int[Y][X];
+        isVisited = new boolean[Y][X];
 
-        for (int i = 0; i < n; i++) {
-            char[] input = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = input[j] - '0';
+        for (int y = 0; y < Y; y++) {
+            String input = br.readLine();
+            for (int x = 0; x < X; x++) {
+                map[y][x] = input.charAt(x) - '0';
             }
         }
 
-        bfs(0, 0);
+        int result = bfs();
 
-        System.out.println(map[n-1][m-1]);
+        System.out.println(result);
 
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
-    public static int bfs(int x, int y) {
+    public static int bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x,y});
-        isVisited[x][y] = true;
-
-        int count = 1;
+        queue.offer(new int[]{0, 0, 1}); // x, y, dist
+        isVisited[0][0] = true;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int cx = cur[0];
             int cy = cur[1];
+            int dist = cur[2];
+
+            if (cx == X - 1 && cy == Y - 1) {
+                return dist;
+            }
 
             for (int d = 0; d < 4; d++) {
                 int nx = cx + dx[d];
                 int ny = cy + dy[d];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (map[nx][ny] == 1 && !isVisited[nx][ny]) {
-                        queue.offer(new int[]{nx, ny});
-                        isVisited[nx][ny] = true;
-                        map[nx][ny] = map[cx][cy] + 1;
-                    }
+                if (nx < 0 || ny < 0 || nx >= X || ny >= Y) {
+                    continue;
                 }
+
+                if (map[ny][nx] == 0 || isVisited[ny][nx]) {
+                    continue;
+                }
+
+                isVisited[ny][nx] = true;
+                queue.offer(new int[] {nx, ny, dist + 1});
             }
         }
-
-        return count;
+        return -1;
     }
 }
+
