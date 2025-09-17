8a6f292ffc7bb48488c93075cd33d453ab63b4cc
최민우 <alsdn4590@gmail.com>
2025-09-18 08:25:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,27 +1,17 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
+        int[] dp = new int[1001];
         dp[1] = 1;
-        if (N >= 2) dp[2] = 2;
-        if (N >= 3) dp[3] = 3;
-        if (N >= 4) dp[4] = 5;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 
-        for (int i = 5; i <= N; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
-        }
-
-        bw.write(dp[N] + "");
-
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(dp[n]);
 
     }
 }
