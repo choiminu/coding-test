a58d7030bad64a078bd9e144e923d6be6e0e8fc5
최민우 <alsdn4590@gmail.com>
2025-09-18 09:23:29 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -12,57 +11,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int K;
-    static int cnt;
-
     static List<List<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
+    static boolean[] isVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        K = Integer.parseInt(br.readLine());
-        visited = new boolean[N + 1];
+        int N = Integer.parseInt(br.readLine());
+        isVisited = new boolean[N + 1];
+
+        int S = Integer.parseInt(br.readLine());
 
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < K; i++) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < S; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int u = Integer.parseInt(token.nextToken());
-            int v = Integer.parseInt(token.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
 
-        cnt = 1;
-
-        dfs(1);
-
-        bw.write((cnt - 1)+"");
+        System.out.println(bfs(1));
 
         br.close();
         bw.flush();
         bw.close();
-
     }
 
-    private static void dfs(int node) {
-        visited[node] = true;
-        for (int i = 0; i < graph.get(node).size(); i++) {
-            int newNode = graph.get(node).get(i);
-            if (!visited[newNode]) {
+    public static int bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        isVisited[start] = true;
+
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (Integer next : graph.get(cur)) {
+                if (isVisited[next]) continue;
+                isVisited[next] = true;
+                queue.offer(next);
                 cnt++;
-                dfs(newNode);
             }
         }
+
+        return cnt;
     }
 
 
 }
+
