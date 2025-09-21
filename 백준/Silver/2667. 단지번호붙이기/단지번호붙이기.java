67ccb832423f210e860dcb6e48bef9af6bc9f04a
최민우 <alsdn4590@gmail.com>
2025-09-22 08:54:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,67 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
-class Main {
+public class Main {
 
     static int N;
+    static int[][] map;
+    static boolean[][] isVisited;
 
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    static int[][] map;
-    static boolean[][] isVisited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-
         map = new int[N][N];
         isVisited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-            char[] input = br.readLine().toCharArray();
+            String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = input[j] - '0';
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
-        int total = 0;
-        List<Integer> result = new ArrayList<>();
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < N; x++) {
-                if (map[y][x] == 1 && !isVisited[y][x]) {
-                    result.add(bfs(x, y));
-                    total++;
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 1 && !isVisited[i][j]) {
+                    count++;
+                    list.add(bfs(j,i));
                 }
             }
         }
 
-        Collections.sort(result);
-        System.out.println(total);
-        System.out.println(result.stream().map(String::valueOf).collect(Collectors.joining("\n")));
+        System.out.println(count);
+        Collections.sort(list);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
-    private static int bfs(int x, int y) {
+    public static int bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x,y});
+        queue.offer(new int[]{x, y});
         isVisited[y][x] = true;
 
-        int area = 1;
-
+        int size = 1;
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int cx = cur[0];
@@ -67,17 +71,20 @@ class Main {
                 int nx = cx + dx[d];
                 int ny = cy + dy[d];
 
-                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-                    if (map[ny][nx] == 1 && !isVisited[ny][nx]) {
-                        queue.offer(new int[]{nx, ny});
-                        isVisited[ny][nx] = true;
-                        area++;
-                    }
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+                    continue;
+                }
+                if (map[ny][nx] == 0 || isVisited[ny][nx]) {
+                    continue;
                 }
 
+                size++;
+                queue.offer(new int[]{nx, ny});
+                isVisited[ny][nx] = true;
             }
         }
-        return area;
+
+        return size;
     }
 }
 
