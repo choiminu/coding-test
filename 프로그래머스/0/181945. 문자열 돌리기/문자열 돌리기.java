a98ae583df6a53c88cd4da2b831ff7251e85fec3
최민우 <alsdn4590@gmail.com>
2025-09-22 07:40:24 +0900

    [level 0] Title: 문자열 돌리기, Time: 180.20 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.next().chars().forEach(ch -> System.out.println((char)ch));
+    }
+}
