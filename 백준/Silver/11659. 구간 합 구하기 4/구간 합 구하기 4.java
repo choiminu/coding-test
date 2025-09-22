1f98e58ae2adac6265b45e8c311384bcb7124974
최민우 <alsdn4590@gmail.com>
2025-09-23 08:48:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1384 ms, Memory: 95864 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N + 1];
-		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
-			arr[0] = 0;
-			arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			sb.append(arr[end] - arr[start - 1]).append("\n");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        dp[1] = A[0];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = A[i] + dp[i - 1];
+        }
+
+        while (T --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            if (left == 1) {
+                System.out.println(dp[right]);
+            } else {
+                System.out.println(dp[right] - dp[left - 1]);
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
