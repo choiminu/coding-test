3ba0a66736fe8ed8e8efd7e0da372f8bd33a32e7
최민우 <alsdn4590@gmail.com>
2025-09-23 10:55:20 +0900

    [Silver I] Title: 정수 삼각형, Time: 316 ms, Memory: 29520 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,51 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] dp = new int[N][N];
+        int[][] A = new int[N][N+1];
 
-        for (int row = 0; row < N; row++) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
-            for (int col = 0; col <= row; col++) {
-                dp[row][col] = Integer.parseInt(token.nextToken());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        int[][] dp = new int[N][N+1];
+        dp[0][0] = A[0][0];
 
         for (int i = 1; i < N; i++) {
             for (int j = 0; j <= i; j++) {
 
                 if (j == 0) {
-                    dp[i][j] += dp[i - 1][j];
-                } else if (j == i) {
-                    dp[i][j] += dp[i - 1][j - 1];
+//                    dp[i][j] = Math.max(A[i][j] + dp[i-1][j], A[i][j] + dp[i-1][j+1]);
+                    dp[i][j] = A[i][j] + dp[i-1][j];
                 } else {
-                    dp[i][j] += Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
+                    dp[i][j] = Math.max(A[i][j] + dp[i-1][j-1], A[i][j] + dp[i-1][j]);
                 }
+
             }
         }
 
-        int max = 0;
-        for (int val : dp[N - 1]) {
-            max = Math.max(max, val);
-        }
+        int result = Arrays.stream(dp)
+                .flatMap(i -> Arrays.stream(i).boxed()).mapToInt(i -> i).max().getAsInt();
 
-        System.out.println(max);
+        System.out.println(result);
 
         br.close();
         bw.flush();
         bw.close();
     }
 }
-
