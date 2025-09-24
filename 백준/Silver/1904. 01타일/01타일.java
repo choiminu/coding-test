c01e5ecebf2b62925ae37586376f8d86f9e86492
최민우 <alsdn4590@gmail.com>
2025-09-25 08:31:09 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,27 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    static final int MOD = 15746;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
 
+        int[] dp = new int[N + 1];
         dp[1] = 1;
         if (N >= 2) dp[2] = 2;
 
         for (int i = 3; i <= N; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+            dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
         }
 
         System.out.println(dp[N]);
 
-
         br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
-
