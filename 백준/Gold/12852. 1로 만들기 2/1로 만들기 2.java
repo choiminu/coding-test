01aee81e264d3f8dc61cf1fa9e12ab07b6dfa1d7
최민우 <alsdn4590@gmail.com>
2025-09-25 07:26:46 +0900

    [Gold V] Title: 1로 만들기 2, Time: 132 ms, Memory: 22288 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,34 +1,34 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 사용자에게 N을 입력받는다.
+        // 목표 숫자 N
         int N = Integer.parseInt(br.readLine());
 
-        // 현재 i번째 위치에서 1을 만들기 위해 필요한 최소한의 연산을 저장
+        // dp[i] : 정수 i를 1로 만드는 최소 연산 횟수
         int[] dp = new int[N + 1];
+
+        // prev[i] : i를 최소 연산으로 만들 때 바로 이전 값
         int[] prev = new int[N + 1];
 
         for (int i = 2; i <= N; i++) {
             dp[i] = dp[i - 1] + 1;
             prev[i] = i - 1;
 
-            if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
                 dp[i] = dp[i / 2] + 1;
                 prev[i] = i / 2;
             }
 
-            if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
                 dp[i] = dp[i / 3] + 1;
                 prev[i] = i / 3;
             }
@@ -36,20 +36,21 @@ public class Main {
 
         System.out.println(dp[N]);
 
-        List<Integer> path = new ArrayList<>();
-        int current = N;
+        List<Integer> list = new ArrayList<>();
+        int currentIdx = N;
 
-        while (current != 0) {
-            path.add(current);
-            current = prev[current];
+        while (currentIdx != 0) {
+            list.add(currentIdx);
+            currentIdx = prev[currentIdx];
         }
 
-        path.stream()
-                .sorted(Collections.reverseOrder())
-                .forEach(i -> System.out.print(i + " "));
+        for (Integer i : list) {
+            bw.write(i + " ");
+        }
 
         br.close();
         bw.flush();
         bw.close();
     }
+
 }
