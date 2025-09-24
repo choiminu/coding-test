34888185a63f9fc439f95450792948a0c9230875
최민우 <alsdn4590@gmail.com>
2025-09-25 07:49:49 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 140 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,39 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // 사용자에게 수열의 크기를 입력받는다.
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] dp = new int[N];
 
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        // 수열을 저장할 배열 선언.
+        int[] A = new int[N];
+
+        // 사용자에게 수열의 원소를 입력받아 배열에 저장한다.
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(token.nextToken());
-            dp[i] = A[i];
+            A[i] = Integer.parseInt(st.nextToken());
         }
+
+        // dp[i] : i번째 원소를 마지막으로 하는 부분 수열의 최대 합
+        int[] dp = new int[N];
+        dp[0] = A[0];
 
         for (int i = 1; i < N; i++) {
+            int maxPrev = 0;
             for (int j = 0; j < i; j++) {
                 if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
+                    maxPrev = Math.max(maxPrev, dp[j]);
                 }
             }
+            dp[i] = A[i] + maxPrev;
         }
 
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            max = Math.max(max, dp[i]);
-        }
-
-        System.out.println(max);
+        System.out.println(Arrays.stream(dp).max().getAsInt());
 
         br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
-
