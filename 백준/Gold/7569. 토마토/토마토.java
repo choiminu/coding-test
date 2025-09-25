7ff4b37bac90f9ab0922974c485eca6801dd1838
최민우 <alsdn4590@gmail.com>
2025-09-25 09:22:42 +0900

    [Gold V] Title: 토마토, Time: 732 ms, Memory: 148972 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,105 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    static int N, M, L;
-
+    static int X,Y,Z;
     static int[][][] box;
-    static boolean[][][] isVisited;
-
-    static int[] dx = {0, 0, -1, 1, 0, 0};
-    static int[] dy = {0, 0, 0, 0, -1, 1};
-    static int[] dz = {-1, 1, 0, 0, 0, 0};
 
     static Queue<int[]> queue = new LinkedList<>();
+    static int[] dx = {-1, 1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(token.nextToken());
-        M = Integer.parseInt(token.nextToken());
-        L = Integer.parseInt(token.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+        Z = Integer.parseInt(st.nextToken());
+        box = new int[Z][Y][X];
 
-        box = new int[L][M][N];
-        isVisited = new boolean[L][M][N];
-
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < M; j++) {
-                token = new StringTokenizer(br.readLine());
-                for (int k = 0; k < N; k++) {
-                    box[i][j][k] = Integer.parseInt(token.nextToken());
-                }
-            }
-        }
-
-        for (int z = 0; z < L; z++) {
-            for (int y = 0; y < M; y++) {
-                for (int x = 0; x < N; x++) {
+        for (int z = 0; z < Z; z++) {
+            for (int y = 0; y < Y; y++) {
+                String[] input = br.readLine().split(" ");
+                for (int x = 0; x < X; x++) {
+                    box[z][y][x] = Integer.parseInt(input[x]);
                     if (box[z][y][x] == 1) {
-                        queue.offer(new int[]{x, y, z});
-                        isVisited[z][y][x] = true;
+                        queue.offer(new int[]{x, y, z, 1});
                     }
                 }
             }
         }
 
         bfs();
-
-        boolean hasZero = false;
-        int maxDay = 0;
-
-        for (int[][] layer : box) {
-            for (int[] arr : layer) {
-                for (int num : arr) {
-                    if (maxDay < num) {
-                        maxDay = num;
+        
+        int max = 0;
+        for (int[][] ints : box) {
+            for (int[] anInt : ints) {
+                for (int i : anInt) {
+                    if (i == 0) {
+                        System.out.println(-1);
+                        return;
                     }
-
-                    if (num == 0) {
-                        hasZero = true;
-                        break;
-                    }
+                    max = Math.max(max, i);
                 }
             }
         }
 
-        if (hasZero) {
-            System.out.println(-1);
-        } else {
-            System.out.println(maxDay - 1);
-        }
+        System.out.println(max-1);
 
-
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
     public static void bfs() {
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-
             int cx = cur[0];
             int cy = cur[1];
             int cz = cur[2];
+            int time = cur[3];
 
             for (int d = 0; d < 6; d++) {
                 int nx = cx + dx[d];
                 int ny = cy + dy[d];
                 int nz = cz + dz[d];
 
-                if (nx >= 0 && ny >= 0 && nz >= 0 && nx < N && ny < M && nz < L) {
-                    if (box[nz][ny][nx] == 0 && !isVisited[nz][ny][nx]){
-                        queue.offer(new int[]{nx, ny, nz});
-                        isVisited[nz][ny][nx] = true;
-                        box[nz][ny][nx] = box[cz][cy][cx] + 1;
-                    }
-                }
+                if (nx < 0 || ny < 0 || nz < 0 || nx >= X || ny >= Y || nz >= Z) continue;
+                if (box[nz][ny][nx] != 0) continue;
+
+                box[nz][ny][nx] = time + 1;
+                queue.offer(new int[]{nx, ny, nz, time + 1});
             }
         }
-
     }
+
 }
