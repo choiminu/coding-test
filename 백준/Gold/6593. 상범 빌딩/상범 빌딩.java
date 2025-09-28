1cdffc5a6ce9adee910fda3005ac4be2de0dc7e3
최민우 <alsdn4590@gmail.com>
2025-09-29 07:53:06 +0900

    [Gold V] Title: 상범 빌딩, Time: 180 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,84 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
-class Main {
+public class Main {
 
-    static int L;
-    static int R;
-    static int C;
-    static int minTime = 99;
+    static int X, Y, Z;
 
-    static char[][][] building;
+    static char[][][] map;
     static boolean[][][] isVisited;
 
-    static Queue<int[]> queue;
+    static Queue<int[]> player;
 
-    // 동 서 남 북 상 하
-    static int[] dx = {1, -1, 0, 0, 0, 0};
-    static int[] dy = {0, 0, 1, -1, 0, 0};
-    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[] dx = {-1, 1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while (true) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Z = Integer.parseInt(st.nextToken());
+            Y = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken());
 
-            L = Integer.parseInt(token.nextToken());
-            R = Integer.parseInt(token.nextToken());
-            C = Integer.parseInt(token.nextToken());
-
-            if (L == 0 && R == 0 && C == 0) {
+            if (Z == 0 && Y == 0 && X == 0) {
                 break;
             }
 
-            building = new char[L][R][C];
-            isVisited = new boolean[L][R][C];
-            queue = new LinkedList<>();
+            player = new LinkedList<>();
+            map = new char[Z][Y][X];
+            isVisited = new boolean[Z][Y][X];
 
-            for (int z = 0; z < L; z++) {
-                for (int y = 0; y < R; y++) {
-                    char[] input = br.readLine().toCharArray();
-                    for (int x = 0; x < C; x++) {
-                        building[z][y][x] = input[x];
-                        if (input[x] == 'S') {
-                            queue.offer(new int[]{x, y, z, 0});
+            for (int z = 0; z < Z; z++) {
+                for (int y = 0; y < Y; y++) {
+                    char[] temp = br.readLine().toCharArray();
+                    for (int x = 0; x < X; x++) {
+                        map[z][y][x] = temp[x];
+                        if (map[z][y][x] == 'S') {
+                            player.offer(new int[]{x, y, z, 0});
                         }
                     }
                 }
                 br.readLine();
             }
 
-            int time = bfs();
+            int result = bfs();
 
-            if (time != -1) {
-                System.out.println("Escaped in "+ time +" minute(s).");
+            if (result == -1) {
+                bw.write("Trapped!\n");
             } else {
-                System.out.println("Trapped!");
+                bw.write("Escaped in " + result + " minute(s).\n");
             }
-
         }
 
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
-    private static int bfs() {
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
+
+    public static int bfs() {
+        while (!player.isEmpty()) {
+            int[] cur = player.poll();
             int cx = cur[0];
             int cy = cur[1];
             int cz = cur[2];
             int time = cur[3];
 
-            if (building[cz][cy][cx] == 'E') {
+            if (map[cz][cy][cx] == 'E') {
                 return time;
             }
 
@@ -87,15 +79,20 @@ class Main {
                 int ny = cy + dy[d];
                 int nz = cz + dz[d];
 
-                if (nx >= 0 && ny >= 0 && nz >= 0 && nx < C && ny < R && nz < L) {
-                    if (building[nz][ny][nx] != '#' && !isVisited[nz][ny][nx]) {
-                        queue.offer(new int[]{nx, ny, nz, time + 1});
-                        isVisited[nz][ny][nx] = true;
-                    }
+                if (nx < 0 || ny < 0 || nz < 0 || nx >= X || ny >= Y || nz >= Z) {
+                    continue;
                 }
+                if (map[nz][ny][nx] == '#' || isVisited[nz][ny][nx]) {
+                    continue;
+                }
+
+                isVisited[nz][ny][nx] = true;
+                player.offer(new int[]{nx, ny, nz, time + 1});
             }
         }
+
         return -1;
     }
-}
 
+
+}
