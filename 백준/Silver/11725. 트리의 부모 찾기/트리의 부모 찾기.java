2cdb94a2d541b174c8cb41c8898fe27b53b5d5c1
최민우 <alsdn4590@gmail.com>
2025-09-29 09:47:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1060 ms, Memory: 96352 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,54 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<List<Integer>> graph = new ArrayList<>();
-    static int[] parent;
-    static boolean[] visited;
+    static List<List<Integer>> list = new ArrayList<>();
+    static List<List<Integer>> parent = new ArrayList<>();
+    static boolean[] isVisited;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        parent = new int[N + 1];
-        visited = new boolean[N + 1];
+
+        isVisited = new boolean[N + 1];
 
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < N - 1; i++) {
-            StringTokenizer token = new StringTokenizer(br.readLine());
-
-            int u = Integer.parseInt(token.nextToken());
-            int v = Integer.parseInt(token.nextToken());
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+        for (int i = 0; i <= N; i++) {
+            parent.add(new ArrayList<>());
         }
+
+
+        for (int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
 
         dfs(1);
 
-        for (int i = 2; i < parent.length; i++) {
-            System.out.println(parent[i]);
+        for (int i = 2; i < parent.size(); i++) {
+            System.out.println(parent.get(i).get(0));
         }
 
         br.close();
     }
 
-    public static void dfs(int index) {
-        visited[index] = true;
-
-        for (int i : graph.get(index)) {
-            if (!visited[i]) {
-                parent[i] = index;
-                dfs(i);
+    public static void dfs(int node) {
+        isVisited[node] = true;
+        for (int next : list.get(node)) {
+            if (!isVisited[next]) {
+                parent.get(next).add(node);
+                dfs(next);
             }
         }
     }
-}
 
+}
