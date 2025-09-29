81afe251702a9cef39f4c652fb495bdd079ef19d
최민우 <alsdn4590@gmail.com>
2025-09-29 09:00:58 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 144 ms, Memory: 22220 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,43 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.Arrays;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int MOD = 1_000_000_009;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+        int mod = 1_000_000_009;
 
-        int[] inputs = new int[N];
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            inputs[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, inputs[i]);
-        }
-
-        int[] dp = new int[max + 1];
+        long[] dp = new long[1_000_001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
-
-        for (int i = 4; i <= max; i++) {
-            dp[i] = ((dp[i - 1] + dp[i - 2]) % MOD + dp[i - 3]) % MOD;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = (dp[i - 3] + dp[i - 2] + dp[i-1]) % mod;
         }
 
-        for (int i : inputs) {
-            System.out.println(dp[i]);
+        while (t --> 0) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n] % mod);
         }
-
-
 
         br.close();
     }
 
 }
-
