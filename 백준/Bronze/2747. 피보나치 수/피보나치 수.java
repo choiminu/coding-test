16117187e223c04d69deebb67970c8f3401f3c17
최민우 <alsdn4590@gmail.com>
2025-09-30 09:56:10 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,32 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] dp = new int[46];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 2;
 
         int N = Integer.parseInt(br.readLine());
-
-        for (int i = 4; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
 
-        bw.write(dp[N] + "");
+        System.out.println(dp[N]);
 
         br.close();
-        bw.flush();
-        bw.close();
     }
+
 
 }
