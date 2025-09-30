4aa7e969b635917ad360a4465e92cb0b57a48238
최민우 <alsdn4590@gmail.com>
2025-09-30 10:21:10 +0900

    [Silver I] Title: 오르막 수, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,32 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
+    final static int MOD = 10_007;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
         int[][] dp = new int[N + 1][10];
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1;
-        }
+        Arrays.fill(dp[1], 1);
 
         for (int i = 2; i <= N; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = j; k < 10; k++) {
-                    dp[i][j] += dp[i - 1][k] % 10007;
-                }
+            int suffix = 0; 
+            for (int j = 9; j >= 0; j--) {
+                suffix = (suffix + dp[i - 1][j]) % MOD;
+                dp[i][j] = suffix;
             }
         }
 
-        int answer = 0;
-        for (int i = 0; i < 10; i++) {
-            answer = (answer + dp[N][i]) % 10007;
-        }
-
-        System.out.println(answer);
+        int ans = 0;
+        for (int v : dp[N]) ans = (ans + v) % MOD;
+        System.out.println(ans);
 
         br.close();
     }
