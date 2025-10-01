4627eed887655f67c1dd2c587d45962c59c60564
최민우 <alsdn4590@gmail.com>
2025-10-01 09:05:01 +0900

    [level 0] Title: 배열 만들기 5, Time: 9.97 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        return Arrays.stream(intStrs).mapToInt(str -> Integer.parseInt(str.substring(s, s + l))).filter(str -> str > k).toArray();
+    }
+}
