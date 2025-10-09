43043edaa2a8a5bdd8f4ae8e1449a67e4d00b83f
최민우 <alsdn4590@gmail.com>
2025-10-09 13:09:51 +0900

    Time: 756 ms (63.2%), Space: 0B (100%) - LeetHub

---
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -0,0 +1,11 @@
+select
+    p.product_id,
+    ifnull(round(sum(p.price * units) / sum(units), 2), 0) as average_price
+from
+    Prices p
+left join
+    UnitsSold u 
+    on p.product_id = u.product_id 
+    and u.purchase_date >= p.start_date and u.purchase_date <= p.end_date
+group by
+    p.product_id
