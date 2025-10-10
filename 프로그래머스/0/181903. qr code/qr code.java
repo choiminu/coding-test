bab8c845295620bbe03984afd1429fe3c9717f2d
최민우 <alsdn4590@gmail.com>
2025-10-11 08:01:52 +0900

    [level 0] Title: qr code, Time: 13.01 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,6 @@
+import java.util.stream.*;
+class Solution {
+    public String solution(int q, int r, String code) {
+        return IntStream.range(0, code.length()).filter(i -> i % q == r).mapToObj(i -> code.charAt(i) + "").collect(Collectors.joining());
+    }
+}
