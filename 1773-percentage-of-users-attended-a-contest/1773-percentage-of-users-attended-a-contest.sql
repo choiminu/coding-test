6f2805e2f77ce5e91eb4cc0ce6c2e615b43284e2
최민우 <alsdn4590@gmail.com>
2025-10-13 10:13:25 +0900

    Time: 818 ms (97.34%), Space: 0B (100%) - LeetHub

---
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,11 @@
+select
+    contest_id,
+    round(count(r.user_id) / (select count(*) from Users)  * 100, 2) as percentage
+from
+    Users u
+join
+    Register r on u.user_id = r.user_id
+group by
+    contest_id
+order by
+    percentage desc, contest_id
