b519f8586e236fcff7326102168395df1e6f1c01
최민우 <alsdn4590@gmail.com>
2025-10-16 11:02:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,20 +1,17 @@
 class Solution {
-    public int solution(long num) {
-        int answer = 0;
-
-        while (num != 1) {
-            if (num % 2 == 0) {
-                num /= 2;
-            } else {
-                num = num * 3 + 1;
+    public int solution(int num) {
+        long n = num;         
+        int count = 0;
+        
+        while (n != 1 && count < 500) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {  
+                n = n * 3 + 1;
             }
-            answer++;
-
-            if (answer >= 500) {
-                return -1;
-            }
+            count++;
         }
-
-        return answer;
+        
+        return (n == 1) ? count : -1;
     }
 }
