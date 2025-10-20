967963cd871db6e103c8283b07067c4546cac7bd
최민우 <alsdn4590@gmail.com>
2025-10-20 09:43:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.45 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,20 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
-        
-        if (s.length() == 4 || s.length() == 6) {
-            for (char ch : s.toCharArray()){
-                if (ch >= '0' && ch <= '9') {
-                    answer = true;
-                } else {
-                    answer = false;
-                    break;
-                }
-            }
-        } else {
-            answer = false;
-        }
-        
-        return answer;
+        return (s.length() == 4 || s.length() == 6) && !s.matches(".*[a-zA-Z].*");
     }
 }
