253ad3c9cafaeb47f13433cefd00677c8bb210a7
최민우 <alsdn4590@gmail.com>
2025-10-22 09:54:37 +0900

    [level 1] Title: K번째수, Time: 8.96 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,26 +1,7 @@
 import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        
-        for (int row = 0; row < commands.length; row++) {
-            
-            int i = commands[row][0];
-            int j = commands[row][1];
-            int k = commands[row][2];
-            
-            int[] temp = new int[j - i + 1];
-            for (int index = 0; i <= j; i++, index++) {
-                temp[index] = array[i - 1];
-            }
-            
-            Arrays.sort(temp);
-
-            answer[row] = temp[k - 1];
-        
-        }
-        
-        return answer;
+        return Arrays.stream(commands).mapToInt(cmd -> Arrays.stream(array, cmd[0] - 1, cmd[1]).sorted().skip(cmd[2] - 1).findFirst().getAsInt()).toArray();
     }
+    
 }
